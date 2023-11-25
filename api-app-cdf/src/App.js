8bb39ef1c5e65831d326276bd985e9bc61c6f6ac
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2023-11-25 09:32:17 -0300

    Api Simpson

--- api-app-cdf/src/App.js
+++ api-app-cdf/src/App.js
@@ -4,30 +4,39 @@ import { CardCharacter } from './cardCharacter/CardCharacter';
 
 function App() {
   const [characters, setcharacters] = useState([])
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`https://apisimpsons.fly.dev/api/personajes`)
+      const response = await fetch(`https://apisimpsons.fly.dev/api/personajes?limit=5&page=${count}`)
       const data = await response.json()
 
       setcharacters(data.docs)
+      console.log(response)
     }
 
     fetchData()
 
-  }, [])
-
-  console.log(characters);
+  }, [count])
+  const handlerSuma = () => {
+    setCount(count + 1)
+  }
+  const handlerResta = () => {
+    count===0?setCount(count):setCount(count - 1)
+  }
 
   return (
     <div className="App">
-      <h1>Los simpson</h1>
+      <h1 className="title">Los simpson</h1>
+      <section className="btn-count">
+        <button className="btn-handler" onClick={handlerResta}>Página Anterior</button>
+        <button className="btn-handler" onClick={handlerSuma}>Página Siguiente</button>
+      </section>
       <ul className="cardList">
         {characters.map((character) => {
-          console.log(character);
           return (
             <CardCharacter key={character._id} character={character} />
-          )
+            )
         })}
       </ul>
     </div>
